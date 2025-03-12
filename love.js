c0e3be2dc56763bf65d9a2f55de4e086bde54b36
ryynnee <165330964+ryynnee@users.noreply.github.com>
2025-03-13 04:43:43 +0800

    Add files via upload

---
+++ love.js
@@ -0,0 +1,49 @@
+$("#messageState").on("change", (x) => {
+	$(".message").removeClass("openNor").removeClass("closeNor");
+	if ($("#messageState").is(":checked")) {
+		$(".message").removeClass("closed").removeClass("no-anim").addClass("openNor");
+		$(".heart").removeClass("closeHer").removeClass("openedHer").addClass("openHer");
+		$(".container").stop().animate({"backgroundColor": "#f48fb1"}, 2000);
+		console.log("Abrindo");
+	} else {
+		$(".message").removeClass("no-anim").addClass("closeNor");
+		$(".heart").removeClass("openHer").removeClass("openedHer").addClass("closeHer");
+		$(".container").stop().animate({"backgroundColor": "#fce4ec"}, 2000);
+		console.log("fechando");
+	}
+});
+
+$(".message").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+	console.log("Animation End");
+	if ($(".message").hasClass("closeNor"))
+		$(".message").addClass("closed");
+	$(".message").removeClass("openNor").removeClass("closeNor").addClass("no-anim");
+});
+
+$(".heart").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+	console.log("Animation End");
+	if (!$(".heart").hasClass("closeHer"))
+		$(".heart").addClass("openedHer").addClass("beating");
+	else
+		$(".heart").addClass("no-anim").removeClass("beating");
+	$(".heart").removeClass("openHer").removeClass("closeHer");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const heart = document.querySelector(".heart");
+    const message = document.querySelector(".message");
+    const audio = new Audio("holdon.mp3"); // 🎶 Change to your song file
+
+    heart.addEventListener("click", function () {
+        heart.classList.add("clicked"); // Hide heart
+        message.classList.add("open"); // Show letter
+        audio.play(); // Play song
+    });
+
+    message.addEventListener("click", function () {
+        heart.classList.remove("clicked"); // Show heart back
+        message.classList.remove("open"); // Hide letter
+        audio.pause(); // Pause music
+        audio.currentTime = 0; // Reset song
+    });
+});
